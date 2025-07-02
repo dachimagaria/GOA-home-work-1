24bd04f09645b704ea3e4fe9845e7765aa14e850
dachimagaria <dachiarkania023@gmail.com>
2025-07-02 13:19:24 +0400

    1

---
+++ day 01/classwork/classwork.py
@@ -0,0 +1,39 @@
+
+#string
+print("dachi magaria")
+
+#integer
+print(750)
+
+
+
+#float
+
+print(14.70)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#variable 1
+
+city = "kutaisi"
+
+#variable 2
+
+integer = 40
+
+#variable 3
+
+float = 14.7
