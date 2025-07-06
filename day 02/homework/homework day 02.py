baf1b06af67e470cc8cd4def5a451b9aff3c26f1
dachimagaria <dachiarkania023@gmail.com>
2025-07-06 10:47:06 +0400

    Create homework day 02.py

---
+++ day 02/homework/homework day 02.py
@@ -0,0 +1,92 @@
+
+
+#1)შექმენით ორი ცვლადი,თქვენი დავალებაა ამ ორ ცვლადზე მოახდინოთ ყველა მათემატიკური მოქმედება რაც ვისწავლეთ(+ - * /)
+
+num1=456
+num2=123
+print(456 + 123)
+print(456 - 123)
+print(456 * 123)
+print(456 / 123)
+
+Answer1= 579
+Answer2= 333
+Answer3= 56088
+Answer4= 3.707317073170732
+
+#2)
+city="kutaisi"
+tech="phone"
+country="germany"
+item= "glasses"
+
+print(city)
+print(tech)
+print(country)
+print(item)
+
+
+
+num1= 1437
+num2= 69
+num3= 2489
+num4= 4568
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+
+
+
+
+
+float1= 1.90
+float2= 3.3
+float3= 19.10
+float4= 14.7
+
+print(float1)
+print(float2)
+print(float3)
+print(float4)
+
+
+
+
+#სტინგების დამატებას ეწოდება --> კონკატინაცია
+
+name="dachi"
+
+lastname="arqania"
+
+print(name + lastname)
+
+
+
+
+
+name1="dachi"
+name2="giorgi"
+name3="gega"
+name4="merabi"
+name5="ilia"
+
+print(name1 + name2 + name3 + name4 + name5)
+
+
+
+
+float= 19.3
+integer= 677
+print(float + integer)
+print(float * integer)
+print(float - integer)
+
+ჯამი= 696.3
+ნამრავლი= 13066.1
+სხვაობა= -657.7
+
+
+
+
