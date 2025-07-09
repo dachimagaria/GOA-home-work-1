d164e7ee12ab442327bbd6b15c8ca7d6105e94a2
dachimagaria <dachiarkania023@gmail.com>
2025-07-09 10:37:56 +0400

    1

---
+++ day03/homework.py
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+#integer
+num1=120
+num2=80
+sum_of_numbers=num1+num2
+sum_of_numbers=200
+print(sum_of_numbers)
+
+
+
+name="dachi"
+name="goofy lary"
+name="giorgi"
+name="daviti"
+name="jeff"
+print(name)
+
+
+carbrand= "bmw"
+Carbrand= "audi"
+cArbrAND= "bugati"
+carBrand= "lamborghini"
+caRbRand= "aston martin"
+
+
+print(carbrand)
+print(Carbrand)
+print(cArbrAND)
+print(carBrand)
+print(caRbRand)
+
+
+#1)პირველიში შეცდომა არის რადგან ცვლადების დაწყება რიცხვით არ შეიძლება --> ეს არის შეცდომა 2name = "giorgi"
+name = "giorgi"
+#2)მეორეში წერია user{name="bubunauri" ეს შეცდომაა რადგან ჩვენ მოცემული გვაქვს ერთზე მეტი სიტყვა სადაც ვიყენებთ snake_case ამიტომ
+#მარცხენა ხვეული ფრჩხილის მაგივრად უნდა დავწეროთ ქვედა ტირე
+user_name = "bubunauri"
+#3)მესამეში გვაქვს მოცემული user_name = goga  ესეც შეცდომაა რადგან goga-ს არ აქვს სტრინგები მინიჭებული
+user_name = "ugoga"
+#4) user-name--> user_name , axali --> "axali" რადგან user-name უნდა გამოვიყენოთ snake_case ამიტომ უნდა დავწეროთ ქვედა ტირე
+#ხოლო axali-ს არ ქონდა სტირგი
+user_surname ="axali"
+
+
+carbrand= "bmw"
+Carbrand= "audi"
+cArbrAND= "bugati"
+carBrand= "lamborghini"
+caRbRand= "aston martin"
+#სტრინგების დამატებას --> კონკატინაცია ეწოდება
+print("bmw"+"audi"+"bugati"+"lamborghini"+"aston martin")
+
+#ამისთვის არსებობს ორი ხერხი გამრავლება ან კონკატინაციის ფუნქცია
+name= "dachi"
+num1= 10
+print(name*num1)
+print("dachi"+"dachi"+"dachi"+"dachi"+"dachi"+"dachi"+"dachi"+"dachi"+"dachi"+"dachi")
+
+
+
+# integer-ის და string-ის დამატება , გამოკლება და გაყოფა არ შეიძლება შედეგად კი მოქვცემს eror (bug)
